974a505c7be371166ff740e2fd77ef3dffeb8ca8
freddy_prom <FGH84952320206fgh>
2021-09-18 18:48:47 +0300

    что я могу

--- alice_skill/config.py
+++ alice_skill/config.py
@@ -2,6 +2,7 @@ REQUEST_STATE = "session"
 RESPONSE_STATE = "session_state"
 url = ""
 
+
 class IntentsNames:
     gallery = "galery"
     items_list = "items_list"
@@ -11,3 +12,27 @@ class IntentsNames:
     find_excess = "find_excess"
     lets_lern = "lets_lern"
     stop = "stop"
+    skills = "skills"
+
+
+skills = [
+    {"title": "Найди лишнее",
+     "description": "Интересная игра на перевод слов и выявление закономерностей",
+     "image": None,
+     "button": "Найди лишнее"},
+
+    {"title": "Подсказать правило",
+     "description": "Подскажу и объясню правило по татарскому языку",
+     "image": None,
+     "button": "расскажи правило"},
+
+    {"title": "Учить слова",
+     "description": "Ты сможешь прослушать произношение слов по категориям",
+     "image": None,
+     "button": "Учить слова"},
+
+    {"title": "Интересный факт",
+     "description": "Расскажу что нибудь интересное про татарский язык",
+     "image": None,
+     "button": "Интересный факт"},
+]
